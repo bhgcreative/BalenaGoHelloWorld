d6a7285eb69267ab4e766868c4b8186f3ebe78e8
bhgcreative <bhgcreative@gmail.com>
2019-04-08 17:24:31 -0700

    first commit

---
+++ app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, Elora Thea-Jan Green!)\nI would like to welcome you to your portal."))
+	})
+	addr := ":80"
+	fmt.Println("Example app listening on port ", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
