893663af6e9803999fb1fed4b4b5e8b3927c5614
bhgcreative <bhgcreative@gmail.com>
2019-04-08 18:38:11 -0700

    commit

--- app/main.go
+++ app/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, Elora Thea-Jan Green!)\nI would like to welcome you to your portal."))
+		w.Write([]byte("Hello, Elora Thea-Jan Green!!!!!!!)\nI would like to welcome you to your portal."))
 	})
 	addr := ":80"
 	fmt.Println("Example app listening on port ", addr)
